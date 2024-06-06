c3906502e385d5d50e4bc5a4b470adf148695141
moutazmahmoud <moutaz.prof.egy@gmail.com>
2024-06-07 02:48:15 +0300

    BINARY_TREES

--- binary_trees.h
+++ binary_trees.h
@@ -6,10 +6,10 @@
 /* Basic Binary Tree Structure */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
