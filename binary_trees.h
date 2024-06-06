e253e8d2c2462627390d1935bd477f127c008688
moutazmahmoud <moutaz.prof.egy@gmail.com>
2024-06-07 02:50:39 +0300

    BINARY_TREES

--- binary_trees.h
+++ binary_trees.h
@@ -1,29 +1,29 @@
-#ifndef BINARY_TREES#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef BINARY_TREES_H  // Check if BINARY_TREES_H is not defined
+#define BINARY_TREES_H  // Define BINARY_TREES_H to prevent double inclusion
 
-#include <stdlib.h>
+#include <stdlib.h>  // Include standard library for memory functions
 
 /* Basic Binary Tree Structure */
 struct binary_tree_s
 {
-int n;
-struct binary_tree_s *parent;
-struct binary_tree_s *left;
-struct binary_tree_s *right;
+    int n;  // Value of the node
+    struct binary_tree_s *parent;  // Pointer to the parent node
+    struct binary_tree_s *left;  // Pointer to the left child node
+    struct binary_tree_s *right;  // Pointer to the right child node
 };
 
-typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s binary_tree_t;  // Typedef for basic binary tree
 
 /* Binary Search Tree */
-typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s bst_t;  // Typedef for binary search tree
 
 /* AVL Tree */
-typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s avl_t;  // Typedef for AVL tree
 
 /* Max Binary Heap */
-typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s heap_t;  // Typedef for max binary heap
 
 /* Function Prototype */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);  // Function prototype for creating a new node
 
-#endif /* BINARY_TREES_H */_H
+#endif /* BINARY_TREES_H */  // End of include guard
